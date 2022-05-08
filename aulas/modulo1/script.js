b1de25b3f30fa1eb8d738c66f1de19254c37fd2c
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-05-08 01:31:02 -0300

    aula 3

--- aulas/modulo1/script.js
+++ aulas/modulo1/script.js
@@ -12,18 +12,34 @@
 //   return response;
 // }
 
-const precos = [
-  'Crédito',
-  'R$ 200',
-  'R$ 400',
-  'Contas Pagar',
-  'R$ 300',
-  'R$ 400',
-  'Meus dados',
-];
+// const precos = [
+//   'Crédito',
+//   'R$ 200',
+//   'R$ 400',
+//   'Contas Pagar',
+//   'R$ 300',
+//   'R$ 400',
+//   'Meus dados',
+// ];
 
-const precosFiltro = precos.filter((p) => p.includes('R$'));
-console.log(precosFiltro);
+// const precosFiltro = precos.filter((p) => p.includes('R$'));
+// console.log(precosFiltro);
 
-const precosNumeros = precosFiltro.map((preco) => Number(preco.replace('R$ ', '')));
-console.log(precosNumeros);
+// const precosNumeros = precosFiltro.map((preco) => Number(preco.replace('R$ ', '')));
+// console.log(precosNumeros);
+
+const grupoA = 100;
+const grupoB = 200;
+
+if (grupoA > grupoB) {
+  console.log('Grupo A ganhou');
+} else {
+  console.log('Grupo B ganhou');
+}
+
+const vencedor = grupoA > grupoB ? 'Grupo A venceu' : 'Grupo B venceu';
+console.log(vencedor);
+
+const active = true;
+const button = active && 'Butão está ativo';
+console.log(button);
