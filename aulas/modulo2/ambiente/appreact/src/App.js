9800adc7679455ad7a4c87eea9a1c40abd0a3815
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-05-10 23:33:55 -0300

    jsx 1

--- aulas/modulo2/ambiente/appreact/src/App.js
+++ aulas/modulo2/ambiente/appreact/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+const titulo = <h1>Esse é um titulo</h1>;
+
 const App = () => {
-  return <div>Meu App</div>;
+  const random = Math.random();
+  const ativo = false;
+
+  const estiloP = {
+    color: 'blue',
+    fontSize: '2rem',
+  };
+
+  return (
+    <>
+      <p style={estiloP}>Teste</p>
+      {titulo} <p className={ativo ? 'ativo' : 'inativo'}>{random * 1000}</p>
+    </>
+  );
 };
 
 export default App;
