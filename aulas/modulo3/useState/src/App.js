94bcf1d69ba97fa6c082ff26554057532c5e5065
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-06-06 22:04:43 -0300

    useState

--- aulas/modulo3/useState/src/App.js
+++ aulas/modulo3/useState/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
+import ButtonModal from './ButtonModal';
+import Modal from './Modal';
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false);
-  const [dados, setDados] = React.useState({ nome: 'André', idade: 30 });
-
-  function handleClick() {
-    setAtivo(!ativo);
-    setDados({ ...dados, faculdade: 'possui faculdade' });
-  }
+  const [modal, setModal] = React.useState(false);
 
   return (
     <div>
-      <p>{dados.nome}</p>
-      <p>{dados.idade}</p>
-      <p>{dados.faculdade}</p>
-      <button onClick={handleClick}>{ativo ? 'Ativo' : 'Inativo'}</button>
+      <Modal modal={modal} setModal={setModal} />
+      <ButtonModal setModal={setModal} />
     </div>
   );
 };
