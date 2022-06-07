1eab82b65b152623a2fe658c7bb241104ab919ea
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-06-06 23:40:15 -0300

    useState

--- aulas/modulo3/useState/src/App.js
+++ aulas/modulo3/useState/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
-const App = () => {
-  const [contar, setContar] = React.useState(1);
-  const [items, setItems] = React.useState(['Item 1']);
+import Produto from './Produto';
 
-  function handleClick() {
-    setContar((contar) => contar + 1);
-    setItems((items) => [...items, 'Item ' + contar + 1]);
+const App = () => {
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
+
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
   }
 
   return (
     <div>
-      {items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-      <button onClick={handleClick}>{contar}</button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        Notebook
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        Smartphone
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        Tablet
+      </button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 };
