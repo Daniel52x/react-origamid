d92dfbc35930dde32b6f3496d3445c9e80bc9f5d
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-06-10 00:02:48 -0300

    useMemo

--- aulas/modulo3/useMemo/src/App.js
+++ aulas/modulo3/useMemo/src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
+function operacaoLenta() {
+  let c;
+  for (let i = 0; i < 100000000; i++) {
+    c = i + i / 10;
+  }
+  return c;
+}
+
 const App = () => {
-  return <div>App</div>;
+  const [contar, setContar] = React.useState(0);
+  // const valor = React.useMemo(() => {
+  //   const localItem = window.localStorage.getItem('produto');
+  //   console.log('Aconteu o memo');
+  //   return localItem;
+  // }, []);
+  const t1 = performance.now();
+  const valor = React.useMemo(() => {
+    return operacaoLenta();
+  }, []);
+  console.log(performance.now() - t1);
+
+  return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
 };
 
 export default App;
