29bed8e8e574768896cf5982d8eb69889915ed25
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-06-28 00:51:40 -0300

    componets formulario

--- aulas/modulo4/formulario/src/App.js
+++ aulas/modulo4/formulario/src/App.js
@@ -1,31 +1,42 @@
 import React from 'react';
-
-const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+import Checkbox from './Form/Checkbox';
+import Input from './Form/Input';
+import Radio from './Form/Radio';
+import Select from './Form/Select';
 
 const App = () => {
-  const [cores, setCores] = React.useState([]);
-
-  function handleChange({ target }) {
-    if (target.checked) {
-      setCores([...cores, target.value]);
-    } else {
-      setCores(cores.filter((cor) => cor !== target.value));
-    }
-  }
+  const [nome, setNome] = React.useState('');
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('Vermelho');
+  const [linguagens, setLinguagens] = React.useState([]);
+  const [termos, setTermos] = React.useState([]);
 
   return (
     <form>
-      {coresArray.map((cor) => (
-        <label key={cor} style={{ textTransform: 'capitalize' }}>
-          <input
-            type="checkbox"
-            value={cor}
-            checked={cores.includes(cor)}
-            onChange={handleChange}
-          />
-          {cor}
-        </label>
-      ))}
+      <Checkbox
+        options={['Li e aceito os termos']}
+        value={termos}
+        setValue={setTermos}
+      />
+
+      <Checkbox
+        options={['Javascript', 'PHP', 'Ruby']}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
+      <Select
+        options={['smartphone', 'tablet']}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input
+        label="Nome"
+        id="nome"
+        type="text"
+        value={nome}
+        setValue={setNome}
+      />
     </form>
   );
 };
