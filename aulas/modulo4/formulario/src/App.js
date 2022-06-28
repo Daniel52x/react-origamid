aab8f6657e8c9d204593584549c59f9736c72874
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-06-27 22:12:01 -0300

    select form

--- aulas/modulo4/formulario/src/App.js
+++ aulas/modulo4/formulario/src/App.js
@@ -1,40 +1,20 @@
 import React from 'react';
 
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-  });
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-
-  function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
-  }
-
+  const [select, setSelect] = React.useState('');
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <input
-        id="nome"
-        type="text"
-        name="nome"
-        value={form.nome}
-        onChange={handleChange}
-      />
-
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      <button>Enviar</button>
+    <form>
+      <select
+        value={select}
+        onChange={({ target }) => setSelect(target.value)}
+        id="produtos"
+      >
+        <option disabled value="">Selecione...</option>
+        <option value="notebook">Notebook</option>
+        <option value="smartphone">Smartphone</option>
+        <option value="tablet">Tablet</option>
+      </select>
+      {select}
     </form>
   );
 };
