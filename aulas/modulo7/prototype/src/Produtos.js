c284b838870b681761b7aaf73dc06c05e1c175f1
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-10-18 00:07:33 -0300

    classes

---
+++ aulas/modulo7/prototype/src/Produtos.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Produtos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contar: 0,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    
+  }
+
+  handleClick() {
+    this.setState((state) => ({
+      contar: state.contar + 1,
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.titulo}</h1>
+        <p>{this.state.contar}</p>
+        <button onClick={this.handleClick()}>Adicionar</button>
+      </div>
+    );
+  }
+}
+
+export default Produtos;
