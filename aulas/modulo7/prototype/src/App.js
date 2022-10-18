11d79672d91b9c3214890a3378c2d30a5f8deef8
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-10-17 23:45:55 -0300

    useReducer

--- aulas/modulo7/prototype/src/App.js
+++ aulas/modulo7/prototype/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import Header from './Header';
+import Exemplo from './Exemplo';
+
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action) {
+    case 'aumentar':
+      return state + 1;
+    case 'diminuir':
+      return state - 1;
+    default:
+      throw new Error('');
+  }
+}
 
 const App = () => {
-  const [contar, setContar] = React.useState(0);
+  const [state, dispatch] = React.useReducer(reducer, 0);
+
   return (
     <div>
-      <Header />
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      <button onClick={() => dispatch('aumentar')}>+</button>
+      <button onClick={() => dispatch('diminuir')}>-</button>
+      <Exemplo />
     </div>
   );
 };
--- aulas/modulo7/prototype/src/Contato.js
+++ aulas/modulo7/prototype/src/Contato.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import $ from 'jquery';
 
 const Contato = () => {
   return <div>Contato</div>;
