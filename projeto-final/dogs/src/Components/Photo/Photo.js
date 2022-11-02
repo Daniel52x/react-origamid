66b2283d9891bc3d9f4277ec68e5e49299eef658
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-11-02 19:24:51 -0300

    Foto Página

--- projeto-final/dogs/src/Components/Photo/Photo.js
+++ projeto-final/dogs/src/Components/Photo/Photo.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { PHOTO_GET } from '../../Api';
+import UseFetch from '../../Hooks/UseFetch';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from './PhotoContent';
 
 const Photo = () => {
-  return (
-    <div>Photo</div>
-  )
-}
+  const { id } = useParams();
+  const { data, loading, error, request } = UseFetch();
 
-export default Photo
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url);
+  }, [request, id]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className="container mainContainer">
+        <PhotoContent data={data} single={true} />
+      </section>
+    );
+  else return null;
+};
+
+export default Photo;
