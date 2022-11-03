570473a2ab342912a249dd8c74eee8714d5ac64d
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-11-02 22:29:37 -0300

    graficos

--- projeto-final/dogs/src/Components/User/UserStats.js
+++ projeto-final/dogs/src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import UseFetch from '../../Hooks/UseFetch';
+import { STATS_GET } from '../../Api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      UserStats
-    </div>
-  );
+  const { data, error, loading, request } = UseFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
