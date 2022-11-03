147bfb8e6d73494da6f036142caf2f3c288fa361
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-11-02 21:43:51 -0300

    Head

--- projeto-final/dogs/src/Components/Login/LoginPasswordLost.js
+++ projeto-final/dogs/src/Components/Login/LoginPasswordLost.js
@@ -5,6 +5,7 @@ import UseForm from '../../Hooks/UseForm';
 import UseFetch from '../../Hooks/UseFetch';
 import { PASSWORD_LOST } from '../../Api';
 import Error from '../Helper/Error';
+import Head from '../Helper/Head';
 
 const LoginPasswordLost = () => {
   const login = UseForm();
@@ -23,6 +24,7 @@ const LoginPasswordLost = () => {
 
   return (
     <section>
+      <Head title="Perdeu a Senha" />
       <h1 className="title">Perdeu a senha ?</h1>
       {data ? (
         <p style={{ color: '#4c1' }}>{data}</p>
